8929eb06c2310595b4d1ef48a636b7f06d68487e
tmaulon <thomas.maulon@gmail.com>
2019-12-05 21:39:04 +0100

    add an index page

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class AnimalController extends AbstractController
 {
     /**
-     * @Route("/animal", name="animal")
+     * @Route("/", name="animaux")
      */
     public function index()
     {
