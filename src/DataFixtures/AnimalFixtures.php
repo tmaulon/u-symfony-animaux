920d4e6bf2f183e71f824a999414e205baace5be
tmaulon <thomas.maulon@gmail.com>
2019-12-06 09:36:34 +0100

    add Famille Entity an create relations with animals

--- src/DataFixtures/AnimalFixtures.php
+++ src/DataFixtures/AnimalFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Animal;
+use App\Entity\Famille;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -10,12 +11,29 @@ class AnimalFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $c1 = new Famille();
+        $c1->setLibelle('mammifères')
+            ->setDescription('Animaux vertébrés nourissant leurs petits avec du lait.');
+        $manager->persist($c1);
+
+        $c2 = new Famille();
+        $c2->setLibelle('reptiles')
+            ->setDescription('Animaux vertébrés qui rampent!;');
+        $manager->persist($c2);
+
+        $c3 = new Famille();
+        $c3->setLibelle('poissons')
+            ->setDescription('Animaux invertébrés du monde aquatique.');
+        $manager->persist($c3);
+
+
         $a1 = new Animal();
         $a1->setNom('Chien')
             ->setDescription('Un animal domestique')
             ->setImage('chien.png')
             ->setPoids(10)
-            ->setDangereux(false);
+            ->setDangereux(false)
+            ->setFamille($c1);
         $manager->persist($a1);
 
         $a2 = new Animal();
@@ -23,7 +41,8 @@ class AnimalFixtures extends Fixture
             ->setDescription('Un animal d\'élevage')
             ->setImage('cochon.png')
             ->setPoids(100)
-            ->setDangereux(false);
+            ->setDangereux(false)
+            ->setFamille($c1);
         $manager->persist($a2);
 
         $a3 = new Animal();
@@ -31,7 +50,8 @@ class AnimalFixtures extends Fixture
             ->setDescription('Un animal dangereux')
             ->setImage('Serpent.png')
             ->setPoids(5)
-            ->setDangereux(true);
+            ->setDangereux(true)
+            ->setFamille($c2);
         $manager->persist($a3);
 
         $a4 = new Animal();
@@ -39,7 +59,8 @@ class AnimalFixtures extends Fixture
             ->setDescription('Un animal très dangereux')
             ->setImage('croco.png')
             ->setPoids(500)
-            ->setDangereux(true);
+            ->setDangereux(true)
+            ->setFamille($c2);
         $manager->persist($a4);
 
         $a5 = new Animal();
@@ -47,7 +68,8 @@ class AnimalFixtures extends Fixture
             ->setDescription('Un animal marin très dangereux')
             ->setImage('requin.png')
             ->setPoids(800)
-            ->setDangereux(true);
+            ->setDangereux(true)
+            ->setFamille($c3);
         $manager->persist($a5);
 
 
--- src/Entity/Animal.php
+++ src/Entity/Animal.php
@@ -41,6 +41,12 @@ class Animal
      */
     private $dangereux;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Famille", inversedBy="animaux")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $famille;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -102,6 +108,18 @@ class Animal
     public function setDangereux(bool $dangereux): self
     {
         $this->dangereux = $dangereux;
+
+        return $this;
+    }
+
+    public function getFamille(): ?Famille
+    {
+        return $this->famille;
+    }
+
+    public function setFamille(?Famille $famille): self
+    {
+        $this->famille = $famille;
 
         return $this;
     }
